7c1a47c0b130ca2848be98ce5396a89b8d52a78d
amp <office.partsch@gmail.com>
2014-09-13 19:06:45 +0200

    update file validators to new signatures and error message handling

--- src/main/java/org/validcool/FileValidations.java
+++ src/main/java/org/validcool/FileValidations.java
@@ -8,32 +8,28 @@ public class FileValidations {
     public static <E extends Path> Validator<E> fileExists() {
         return new Validator<>(
                 Files::exists,
-                "file or directory exists",
-                (E value) -> String.format("path \"%s\" does not exist", value)
+                "file or directory \"${actual}\" does not exist"
         );
     }
 
     public static <E extends Path> Validator<E> isFile() {
         return new Validator<>(
                 Files::isRegularFile,
-                "is a regular file",
-                (E value) -> String.format("\"%s\" is not a regular file", value)
+                "\"${actual}\" is not a regular file"
         );
     }
 
     public static <E extends Path> Validator<E> isDirectory() {
         return new Validator<>(
                 Files::isDirectory,
-                "is a directory",
-                (E value) -> String.format("\"%s\" is not a directory", value)
+                "\"${actual}\" is not a directory"
         );
     }
 
     public static <E extends Path> Validator<E> withinPath(Path parent) {
         return new Validator<>(
                 (E value) -> value.startsWith(parent),
-                String.format("is within path \"%s\"", parent),
-                (E value) -> String.format("\"%s\" is not within \"%s\"", value, parent)
+                String.format("\"${actual}\" is not within path \"%s\"", parent)
         );
     }
 
